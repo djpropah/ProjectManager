3608007b783446cf2a567bb269fb0e20d3e19f3b
Denny Kwon <info@tsunami234.com>
2013-01-17 20:59:39 -0500

    removed test code from object model to view controller

--- projectManager/Project.m
+++ projectManager/Project.m
@@ -20,34 +20,7 @@
     self = [super init];
   
     if (self) {
-        Project *testProject = [[Project alloc] init];
-        
-        
-        Assignee *assignee1 = [[Assignee alloc] init];
-        
-        assignee1.assigneeName = @"Robert";
-        Task *task1 = [[Task alloc] init];
-        task1.name = @"task1";
-        
-        Task *task2 = [[Task alloc] init];
-        task2.name = @"task2";
-        
-        assignee1.taskList = [[NSMutableArray alloc] initWithObjects:task1,task2,nil];
-        
-        
-        Assignee *assignee2 = [[Assignee alloc] init];
-        assignee2.assigneeName = @"James";
-        
-        Task *task3 = [[Task alloc] init];
-        task3.name = @"task3";
-        Task *task4 = [[Task alloc] init];
-        task4.name = @"task4";
-        
-        assignee2.taskList = [[NSMutableArray alloc] initWithObjects:task3,task4,nil];
-        
-        testProject.manager = @"Greg";
-        testProject.assignees = [[NSArray alloc] initWithObjects:assignee1, assignee2, nil];
-    }
+            }
     
     
     return self;
